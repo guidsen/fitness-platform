447849bbca2bb9820f94a1cc3dee05dfbe7941b8
Guido Schmitz <guisch95@gmail.com>
2014-11-10 19:45:21 +0100

    Added workout sample data to read from array

--- client/app/controllers/WorkoutController.js
+++ client/app/controllers/WorkoutController.js
@@ -2,7 +2,39 @@ var fitnessApp = angular.module('fitnessApp')
 
 fitnessApp.controller('WorkoutController', function ($scope) {
 
-    $scope.workouts = [{}, {}, {}, {}, {}];
+    $scope.workouts = [{
+        created_at: '23 jul',
+        exercises: 4,
+        sets: 20,
+        gym: 'Fit for Free Den Haag',
+        muscles_used: ['buikspieren', 'benen', 'kuiten'],
+        likes: 2,
+        comments: 2
+    }, {
+        created_at: '21 jul',
+        exercises: 3,
+        sets: 9,
+        gym: 'Fit for Free Forepark',
+        muscles_used: ['benen', 'rug'],
+        likes: 9,
+        comments: 5
+    }, {
+        created_at: '27 aug',
+        exercises: 2,
+        sets: 14,
+        gym: 'Basic Fit Delft',
+        muscles_used: ['buikspieren', 'rug'],
+        likes: 0,
+        comments: 0
+    }, {
+        created_at: '23 jul',
+        exercises: 4,
+        sets: 20,
+        gym: 'Active Health Center Leidscheveen',
+        muscles_used: ['buikspieren', 'borst', 'rug'],
+        likes: 23,
+        comments: 4
+    }];
     console.log($scope.test);
 
 })
